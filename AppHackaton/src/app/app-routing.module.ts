424cc0a3ea0496b1afd3b637c16ce5ae9687bbe2
Armand_D <armand.deshais@mail.com>
2020-01-30 09:21:41 +0100

    changements oubliés

--- AppHackaton/src/app/app-routing.module.ts
+++ AppHackaton/src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
-import {MapBoxComponent} from './map-box/map-box.component';
 
 const routes: Routes = [
     {
         path: '',
         loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-    },  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'signup',
-    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
-  }
+    },
+    {
+        path: 'login',
+        loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+    },
+    {
+        path: 'signup',
+        loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule)
+    }
 
 ];
 
--- AppHackaton/src/app/app.module.ts
+++ AppHackaton/src/app/app.module.ts
@@ -8,10 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MapBoxComponent} from './map-box/map-box.component';
+
 
 @NgModule({
-  declarations: [AppComponent, MapBoxComponent],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
